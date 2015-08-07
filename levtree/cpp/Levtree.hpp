bf910b839cf35a372c764eb4f9dfa07fd728d8e3
Walter Oggioni <oggioni.walter@gmail.com>
2015-08-07 17:09:40 +0200

    ottimizzata la build in modo da non compilare i file più volte

--- levtree/cpp/Levtree.hpp
+++ levtree/cpp/Levtree.hpp
@@ -7,39 +7,9 @@
 #include <string>
 #include <vector>
 
-#include "../common.h"
-#include "../wlevnode.h"
-#include "../levtree_standing.h"
-
-typedef struct wlevtree wlevtree;
-
-struct wlevtree
-{
-    index_t maxsize;
-    byte_t allocated;
-    byte_t torealloc;
-    wlevnode* nodes;
-    index_t node_count;
-    index_t node_size;
-    index_t* entries; //vettore di interi che contiene la posizione di ogni parola nel vettore dei nodi
-    index_t entry_count;
-    index_t entry_size;
-    levtree_standing* standing;
-    byte_t (*checker)(wchar_t key1, wchar_t key2);
-    void (*distance_calculator)(wlevtree* tree, const wchar_t* wordkey, index_t wordlen, index_t* path, index_t pathLength, index_t j);
-};
-
 extern "C"
 {
-void wlevtree_init(wlevtree* tree, wchar_t **words, index_t words_count);
-void wlevtree_free(wlevtree* tree);
-void wlevtree_search(wlevtree* tree, const wchar_t* wordkey,
-		index_t n_of_matches);
-void wlevtree_add_word(wlevtree* tree, const wchar_t *keyword, index_t id);
-levtree_result wlevtree_get_result(wlevtree* tree, index_t index);
-void wlevtree_set_algorithm(wlevtree *tree, levtree_algorithm algo);
-void wlevtree_set_case_sensitive(wlevtree *tree, byte_t boolean);
-
+#include "../wlevtree.h"
 }
 
 
